5bd984e084af6f2d5baa3377c4909c441b57a0bf
Gabriel Liberato De Oliveira Silva <146360818+galiba789@users.noreply.github.com>
2025-02-03 16:14:38 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,10 @@ Route::get('/admin', [admin::class, 'index']);
 Route::get('/admin/produtos', [admin::class, 'produtos'])->name('admin/produtos');
 Route::get('/admin/produtos/cadastro', [admin::class, 'cadastro'])->name('cadastro');
 Route::post('/admin/produtos/cadastro/submit', [admin::class, 'submit'])->name('cadastrar');
+Route::get('admin/produtos/editar/{id}', [admin::class, 'editar'])->name('editar');
+Route::put('admin/produtos/editar/submit/{id}', [admin::class, 'editarSubmit'])->name('atualizar');
+Route::delete('admin/produtos/deletar/{id}', [admin::class, 'destroy'])->name('excluir');
+
+
+
 
