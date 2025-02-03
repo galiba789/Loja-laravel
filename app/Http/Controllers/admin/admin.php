c0f4bfdf9d44399d0b1c5a9b0bc4d6d0f033b53b
Gabriel Liberato De Oliveira Silva <146360818+galiba789@users.noreply.github.com>
2025-02-03 16:13:25 -0300

    Update admin.php

--- app/Http/Controllers/admin/admin.php
+++ app/Http/Controllers/admin/admin.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\produtos;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class admin extends Controller
 {
     public function index() {
+
         return view('admin.index');
     }
 
     public function produtos(){
-        return view('admin.produto');
+        $produtos = produtos::all();
+        return view('admin.produto', compact('produtos'));
     }
     public function cadastro(){
         return view('admin.cadastro');
@@ -25,6 +29,7 @@ class admin extends Controller
                 'preco' => ['required'],
                 'descricao' => ['required', 'max:500'],
                 'imagem' => ['required', 'image', 'mimes:jpeg,png,jpg,gif', 'max:2048'],
+                'estoque' => ['required', 'min:1'],
             ],
             [
                 'nome.required' => 'O nome do produto e obrigatorio',
@@ -36,8 +41,68 @@ class admin extends Controller
                 'imagem.image' => 'Deve ser uma imagem',
                 'imagem.mimes' => 'Formato nao suportado',
                 'imagem.max' => 'a imagem deve ter no maximo :max caracteres',
+                'estoque.required' => 'O valor do estoque e obrigatorio',
+                'estoque.min' => 'Você deve ter no minimo :min produto no estoque',
             ]
         );
+        if ($request->hasFile('imagem')) {
+            $imagemPath = $request->file('imagem')->store('produtos', 'public');
+        } else {
+            return redirect()->back()->with('error', 'Erro ao enviar a imagem.');
+        }
+        $produtos = new produtos();
+        $produtos->nome = $request->nome;
+        $produtos->preco = $request->preco;
+        $produtos->descricao = $request->descricao;
+        $produtos->imagem = $imagemPath ?? null;
+        $produtos->estoque = $request->estoque;
+        $produtos->save();
 
+        return redirect()->route('admin/produtos');
     }
+
+    public function editar($id){
+        $produto = produtos::findOrFail($id);
+        return view('admin.edit', compact('produto'));
+    }
+
+    public function editarSubmit(Request $request, $id){
+        $produto = produtos::findOrFail($id);
+
+        $request->validate([
+            'nome' => 'required|min:2',
+            'preco' => 'required',
+            'descricao' => 'required|max:500',
+            'estoque' => 'required|min:1',
+            'imagem' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+
+        $produto->nome = $request->nome;
+        $produto->preco = $request->preco;
+        $produto->descricao = $request->descricao;
+        $produto->estoque = $request->estoque;
+
+        if ($request->hasFile('imagem')) {
+            $imagemPath = $request->file('imagem')->store('produtos', 'public');
+            $produto->imagem = $imagemPath;
+        }
+
+        $produto->save();
+
+        return redirect()->route('admin/produtos')->with('success', 'Produto atualizado com sucesso!');
+    }
+    public function destroy($id) {
+        $produto = produtos::findOrFail($id);
+
+        if ($produto->imagem) {
+            Storage::disk('public')->delete($produto->imagem);
+        }
+
+
+        $produto->delete();
+
+        return redirect()->route('admin/produtos')->with('success', 'Produto excluído com sucesso!');
+    }
+
 }
